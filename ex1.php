b9a214f01a3dc1cb65733f5aca5056e0a6290661
NitolPranto <18-37136-1@student.aiub.edu>
2021-02-05 18:13:30 +0600

    First commit

---
+++ ex1.php
@@ -0,0 +1,39 @@
+<!Doctype HTML>
+
+<html>
+<head>
+
+<title>My Example</title>
+</head>
+
+<body>
+
+
+<form>
+
+Name:
+ <input type="text" id="fname" placeholder="Enter your name"><br>
+Email: 
+ <input type="text" id="ename" placeholder="Enter your email"><br>
+User Name:
+ <input type="text" id="uname" placeholder="Enter your uname"><br>
+Password:
+<input type="password" id="e" placeholder="Enter your pass"><br>
+Confirm Password:
+<input type="password" id="cp" placeholder="Enter your pass"><br>
+Gender:
+<input type="radio" name="gender" id="male" value="male">
+Male
+<input type="radio" name="gender" id="female" value="female">
+Female
+<input type="radio" name="gender" id="other" value="other">
+Other<br>
+Date of Birth:
+<input type="date"<br><br>
+<input type="submit" value="submit">
+<input type="reset" value="reset">
+</form>
+
+</body>
+
+</html>
---
+++ ex2.php
@@ -0,0 +1,9 @@
+<?php
+
+$text= "Hello World";
+$x = 5;
+$y = 10.5;
+echo "I Love" . $text . "!";
+echo $x+$y;
+
+?>
